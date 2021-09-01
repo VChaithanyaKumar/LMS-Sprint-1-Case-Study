0949b42910d4b38a8f15d90a5d45d5c3e98a84a2
NainaChinna <praveena.vella1999@gmail.com>
2021-09-01 18:20:24 +0530

    new_commit_Entity

---
+++ Sprint1-LMS-CaseStudy/Entities/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint1_LMS_CaseStudy.Entities
+{
+    class Question
+    {
+        public int QuestionId { get; set; }
+        public int QuestionDescription { get; set; }
+        public int CourseId { get; set; }
+
+    }
+}
---
+++ Sprint1-LMS-CaseStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sprint1_LMS_CaseStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
