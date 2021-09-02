4232d57575ec31c32fe3bea56d319a0cb11bc7a0
nimishabhargavi2217@gmail.com <nimishabhargavi2217@gmail.com>
2021-09-02 18:53:54 +0530

    RepositoryReference_dap

--- LearningManagementSystem/LearningManagementSystem.DAL/IRegisterRepository.cs
+++ LearningManagementSystem/LearningManagementSystem.DAL/IRegisterRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearningManagementSystem.Entities;
 
 namespace LMS.DAL
 {
     interface IRegisterRepository
     {
+        void AddingUser(User user);
     }
 }
--- LearningManagementSystem/LearningManagementSystem.DAL/RegisterRepository.cs
+++ LearningManagementSystem/LearningManagementSystem.DAL/RegisterRepository.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 using System.Collections.Generic;
+using LMS.BAL;
+using LearningManagementSystem.Entities;
+
 namespace LMS.DAL
 {
     public class RegisterRepository: IRegisterRepository
@@ -14,4 +17,32 @@ namespace LMS.DAL
         SqlConnection connection = new SqlConnection(@"Data Source=NAINACHINNA\SQLEXPRESS;Initial Catalog=LearningManagementSystem;Integrated Security=True");
         SqlCommand command = null;
     }
+    public void AddingUser(User user)
+    {
+        try
+        {
+            command = new SqlCommand("Addinguser", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+            command.Parameters.AddWithValue("@UserName", user.UserName);
+            command.Parameters.AddWithValue("@UserPassword", user.UserPassword);
+            command.Parameters.AddWithValue("@UserFirstName", user.UserFirstName);
+            command.Parameters.AddWithValue("@UserLastName", user.UserLastName);
+            command.Parameters.AddWithValue("@Gender", user.Gender);
+            command.Parameters.AddWithValue("@UserEmail", user.Email);
+            connection.Open();
+            command.ExecuteNonQuery();
+
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
 }
+
