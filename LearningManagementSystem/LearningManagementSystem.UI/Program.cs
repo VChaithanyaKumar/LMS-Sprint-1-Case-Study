f76bb65de37e509bbb68882c8ac25901a055810e
NainaChinna <praveena.vella1999@gmail.com>
2021-09-03 12:08:37 +0530

    AddingCommentsForConsole

--- LearningManagementSystem/LearningManagementSystem.UI/Program.cs
+++ LearningManagementSystem/LearningManagementSystem.UI/Program.cs
@@ -10,11 +10,15 @@ namespace LearningManagementSystem.UI
         {
             try
             {
+                //object reference for RegisterService
                 RegisterService registerService = null;
+                //object reference for LoginService
                 LoginService loginService = null;
+                //object reference for User
                 User user = null;
                 do
                 {
+
                     Console.WriteLine("1. Existing User");
                     Console.WriteLine("2. New User");
                     Console.WriteLine("3. Exit");
@@ -22,9 +26,10 @@ namespace LearningManagementSystem.UI
                     int ch = int.Parse(Console.ReadLine());
                     switch (ch)
                     {
+                        //if Existing User
                         case 1:
                             {
-
+                                //getting user-type from the user
                                 Console.WriteLine("1. Admin Login");
                                 Console.WriteLine("2. Learner Login");
                                 Console.WriteLine("3. Go Back");
@@ -35,8 +40,10 @@ namespace LearningManagementSystem.UI
                                 {
                                     case 1:
                                         {
+                                            //Admin user login
                                             user = new User();
                                             user.UserType = "Admin";
+                                            //getting email and password from Admin
                                             Console.WriteLine("enter email");
                                             user.UserEmail = Console.ReadLine();
                                             Console.WriteLine("enter password");
@@ -47,8 +54,10 @@ namespace LearningManagementSystem.UI
                                         }
                                     case 2:
                                         {
+                                            //Learner user login
                                             user = new User();
                                             user.UserType = "Learner";
+                                            //getting email and password from Learner
                                             Console.WriteLine("enter email");
                                             user.UserEmail = Console.ReadLine();
                                             Console.WriteLine("enter password");
@@ -65,13 +74,13 @@ namespace LearningManagementSystem.UI
 
                                 break;
                             }
+                        //if new User
                         case 2:
                             {
-
+                                //getting values from user
                                 user = new User();
                                 Console.WriteLine("Enter UserType");
                                 user.UserType = Console.ReadLine();
-
                                 Console.WriteLine("Enter first name");
                                 user.UserFirstName = Console.ReadLine();
                                 Console.WriteLine("Enter last name");
@@ -104,7 +113,9 @@ namespace LearningManagementSystem.UI
             catch (Exception e)
             {
                 FileStream fileStream = new FileStream("G:\\MyCopyLMS\\LearningManagementSystem\\ErrorLog.txt", FileMode.OpenOrCreate);
-                string ErrorMsg= string.Format($"Error Message:{e.Message} Date:{DateTime.Now} Class:Programe");
+                //Add exception details to ErrorLog.txt file.
+                string ErrorMsg = string.Format($"Error Message:{e.Message} Date:{DateTime.Now} Class:Programe");
+                //Add content to ErrorLog.txt file using StreamWriter.
                 StreamWriter streamWriter = new StreamWriter(fileStream);
                 streamWriter.WriteLine(ErrorMsg);
                 streamWriter.Close();
